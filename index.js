f85f0fd94e95838cb724c85ac010a00bcd9a6799
jonasgithub2021 <jonas.barcelona.dota@gmail.com>
2022-06-19 21:49:00 +0800

    update header

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const lightBtn = document.getElementById('light');
 const body = document.body;
 
 const currentTheme = localStorage.getItem('theme')
-
+console.log(currentTheme)
 if(currentTheme){
+    console.log(currentTheme)
     body.classList.add(currentTheme);
+    console.log(currentTheme)
 }
 
 darkBtn.onclick = () => {
@@ -18,5 +20,5 @@ lightBtn.onclick = () => {
     body.classList.replace('dark','light');
     localStorage.setItem('theme','light');
     const currentTheme = localStorage.getItem('theme')
-console.log(currentTheme)
+    console.log(currentTheme)
 }
