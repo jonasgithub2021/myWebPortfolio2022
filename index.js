633c0d0c2f4382c974f34099db37f174a286e43f
jonasgithub2021 <jonas.barcelona.dota@gmail.com>
2022-06-20 23:00:12 +0800

    added images and about section

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+            $('.navbar').removeClass("sticky");
+        }
+
+    })
+   
+});
+
 const darkBtn = document.getElementById('dark');
 const lightBtn = document.getElementById('light');
-const body = document.body;
+// const body = document.body;
+
 
 const currentTheme = localStorage.getItem('theme')
-console.log(currentTheme)
+
 if(currentTheme){
     console.log(currentTheme)
-    body.classList.add(currentTheme);
-    console.log(currentTheme)   
+    // body.classList.add(currentTheme);
+    $("body").addClass(currentTheme);
+    
+      
 }
 else{
-    body.classList.add('dark')
+    $("body").addClass(currentTheme);
 }
 
 darkBtn.onclick = () => {
-body.classList.replace('light','dark');
+    $("body").removeClass('light');
+    $("body").addClass('dark');
+//     localStorage.setItem('theme','light');
+// body.classList.replace('light','dark');
 localStorage.setItem('theme','dark');
-const currentTheme = localStorage.getItem('theme')
-console.log(currentTheme)
+
 }   
 lightBtn.onclick = () => {
-    body.classList.replace('dark','light');
+    // body.classList.replace('dark','light');
+    $("body").removeClass('dark');
+    $("body").addClass('light');
     localStorage.setItem('theme','light');
-    const currentTheme = localStorage.getItem('theme')
-    console.log(currentTheme)
+    
 }
